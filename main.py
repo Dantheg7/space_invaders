2e74f9a99e927d0d2d03b0b63fa4c6ce70a1d177
Danthe <gabritogomez@gmail.com>
2023-03-16 16:15:51 -0400

    add enemy movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,12 +11,21 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("space-invaders (1).png")
 pygame.display.set_icon( icon )
 
+enemy_img = pygame.image.load("ufo.png")
+enemy_x = random.randint(0,100)
+enemy_y = random.randint(50, 150)
+enemy_x_change = 0.2
+enemy_y_change = 20
+
+
 player_ing =  pygame.image.load("nave-espacial.png")
 player_x = 360
 player_y = 500
 player_x_change = 0
 def player(x, y):
     screen.blit(player_ing, (x, y))    
+def enemy(x, y):
+        screen.blit(enemy_img, (x, y)) 
 
 running = True
 while running: 
@@ -24,10 +34,10 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 player_x_change -= 1
 
-            if event.key == pygame.K_d:   
+            if event.key == pygame.K_RIGHT:   
                 player_x_change = 1
 
         if event.type == pygame.KEYUP:
@@ -41,10 +51,23 @@ while running:
         player_x = 0
     elif player_x >= 736:
         player_x = 736
+    enemy_x += enemy_x_change
+    if enemy_x >= 736:
+        enemy_x -= enemy_x_change
+        enemy_x_change = -0.2
+        enemy_y += enemy_y_change
+    elif enemy_x <= 0:
+        enemy_x_change = 0.2
+        enemy_y += enemy_y_change
+    if enemy_y == 450:
+        running = False
+
+   
 
     RGB = (20,80,120)
     screen.fill( RGB )
     player(player_x, player_y)
-    
+    enemy(enemy_x, enemy_y)
+    enemy(enemy_x, enemy_y)
     
     pygame.display.update()
